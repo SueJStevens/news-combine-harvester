b60e9f6860b0056ce198abc764f456d38d2c90f4
SueJStevens <StevensStock@gmail.com>
2018-12-22 22:22:24 -0800

    Progress

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-news-combine-harvester-db";
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/mongo-news-combine-harvester-db", { useNewUrlParser: true });
+
+// Routes
+require("./routes/htmlRoutes")(app);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
